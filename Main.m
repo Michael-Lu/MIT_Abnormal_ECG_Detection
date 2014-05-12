c8d606950c3ccc5adaddbd85db6f1c6486d514b7
Michael-Lu <michael.lu0207@gmail.com>
2014-05-12 10:26:58 +0800

    Finish the Main.m program and fix a sematic bug. Fix the sematic erroror: change ceil(1/num_parts) to num_parts 	modified:   Main.m

--- Main.m
+++ Main.m
@@ -4,13 +4,18 @@ clear all
 clc
 mat_dir = fullfile(pwd,'image_mat_2');
 num_parts = 2;
+PNN_Spares = 5;
 InputDomain = [100 101 103 105 106 108 109 111:119 121:124 200:203 205 207:210 212:215 219:223 228 230:234];
 DataComp = []; %List the number of Normal Records and Abnormal Records in each Person
 %DataList = GatherData(mat_dir);
 %save DataListNew
 load DataListNew
 
+err = zeros(size(InputDomain,2), num_parts);
+sensitivity = zeros(size(InputDomain,2), num_parts);
+specificity = zeros(size(InputDomain,2), num_parts);
 for m = 1:size(InputDomain,2)
+    %% Separate out the Normal and Abnormal Data Sets in a Person.
     targetPer = InputDomain(m);
     targetDataList = DataList( cell2mat(DataList(:,1)) == targetPer, : );
     
@@ -27,16 +32,46 @@ for m = 1:size(InputDomain,2)
         continue
     end
     
-    % Do Cross-Validation Test
-    for n = 1:1%ceil(1/num_parts)
+    %% Perparation to Cross-Validation, Separate out the Testing and Training
+    for n = 1:num_parts
         [NormTrain, NormTest] = DivideTrainTestDataNew(NormalDataList, num_parts, n);
         [AbTrain, AbTest] = DivideTrainTestDataNew(AbNormalDataList, num_parts, n);
         
-    end
-        
+    
+    
+    %% Normalize the Features and Store Them in Numeric Array
     [Train.Normal, Train.AbNormal, NormParameters] = NormalizeFeature_Train( NormTrain(:,5:8), AbTrain(:,5:8) );
     [Test.Normal, Test.AbNormal] = NormalizeFeature_Test(NormParameters, NormTest(:,5:8), AbTest(:,5:8) );
+  
+   %% Classify the Normalized Feature by PNN
+    TrainingSet = [Train.Normal; Train.AbNormal]';
+    TrainingTarget = [zeros(1, size(Train.Normal,1) ) ones(1, size(Train.AbNormal, 1) ) ] + 1;
+    Trained_PNN = Train_PNN(TrainingSet, TrainingTarget, PNN_Spares);
+    
+    TestRslt.Normal = vec2ind( sim(Trained_PNN, Test.Normal') );
+    TestRslt.AbNormal = vec2ind( sim(Trained_PNN, Test.AbNormal') );
+    
+    % Make sure test_normal_rslt and test_abnormal_rslt are both 1-dimension vectors
+    assert(size(TestRslt.Normal,1) == 1);
+    assert(size(TestRslt.AbNormal,1) == 1);
+    %% Analyze the Error Rate of Each Result
+
+    display('---normal to abnormal-----');
+    NormTest(TestRslt.Normal == 2, :)
+
+    display('---abnormal to normal-----');
+    AbTest(TestRslt.AbNormal == 1, :)
+
+    display('>>>error rate<<<')
+    err(m,n) =  (sum(TestRslt.Normal == 2) + sum(TestRslt.AbNormal == 1) )/(size(TestRslt.Normal,2)+size(TestRslt.AbNormal,2) ) *100;
+    sensitivity(m,n) = (1-sum(TestRslt.AbNormal == 1)/size(TestRslt.AbNormal,2) ) *100;
+    specificity(m,n) = (1-sum(TestRslt.Normal == 2)/size(TestRslt.Normal,2) ) *100;
+    
+    end
     
     
 end
+
+display('---Person NormalTrain NormalTest AbnormalTrain AbnormalTest---')
+DataComponent()
 
